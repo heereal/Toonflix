774b98d8f6bd1b1cc8aef4a6ad4e5cb0b70ee62d
heeryoung <winterbooks@naver.com>
2024-05-06 16:50:01 +0900

    feat: FutureBuilder 사용한 api fetching 구현

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,34 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:toonflix/models/webtoon_model.dart';
 import 'package:toonflix/services/api_service.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  List<WebtoonModel> webtoons = [];
-  bool isLoading = true;
-
-  // api 데이터를 호출하는 함수
-  void waitForWebtoons() async {
-    // static 지정을 하면 'ApiService' 클래스 자체가 해당 함수를 소유하는 형태가 됨
-    // 따라서 인스턴스 생성 없이 함수를 호출할 수 있음
-    webtoons = await ApiService.getTodaysToons();
-    isLoading = false;
-    setState(() {}); // UI 새로고침
-  }
-
-  @override
-  // initState는 build 메소드가 호출이 되기전에 딱 한 번 호출됨
-  void initState() {
-    super.initState();
-    // waitForWebtoons 메소드 호출 후 데이터를 받기 전에 build 메소드를 먼저 실행함
-    // waitForWebtoons -> build-> 데이터 도착 -> setState -> build
-    waitForWebtoons();
-  }
+  final Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
@@ -42,6 +18,15 @@ class _HomeScreenState extends State<HomeScreen> {
         foregroundColor: Colors.white,
         elevation: 1,
         shadowColor: Colors.black,
+      ),
+      body: FutureBuilder(
+        future: webtoons,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return const Text('There is data!');
+          }
+          return const Text('Loading...');
+        },
       ),
     );
   }
