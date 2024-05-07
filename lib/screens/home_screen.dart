89603861f42c8d426e97a022dc75a28892702985
heeryoung <winterbooks@naver.com>
2024-05-07 23:50:25 +0900

    design: webtoon card

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -23,26 +23,65 @@ class HomeScreen extends StatelessWidget {
         future: webtoons,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            // 여러 항목을 나열하는데 최적화된 위젯
-            return ListView.separated(
-              scrollDirection: Axis.horizontal,
-              itemCount: snapshot.data!.length,
-              // 모든 아이템을 한번에 build 하지 않으며 사용자가 보고있는 아이템만 build함
-              // 사용자가 보고있지 않은 아이템은 메모리에서 삭제됨
-              itemBuilder: (context, index) {
-                var webtoon = snapshot.data![index];
-                return Text(webtoon.title);
-              },
-              // 각 아이템 사이에 렌더링됨
-              separatorBuilder: (context, index) => const SizedBox(
-                width: 20,
-              ),
+            return Column(
+              children: [
+                const SizedBox(
+                  height: 40,
+                ),
+                // Expanded는 화면의 남은 공간을 차지하는 위젯
+                Expanded(
+                  child: makeList(snapshot),
+                ),
+              ],
             );
           }
           return const Center(
             child: CircularProgressIndicator(),
           );
         },
+      ),
+    );
+  }
+
+  // 메소드로 추출
+  // 여러 항목을 나열하는데 최적화된 위젯
+  // ListView -> ListView.builder -> ListView.separated
+  ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: snapshot.data!.length,
+      // itemBuilder는 리스트뷰의 아이템을 만드는 역할을 함
+      // 모든 아이템을 한번에 build 하지 않으며 사용자가 보고있는 아이템만 build함
+      // 사용자가 보고있지 않은 아이템은 메모리에서 삭제됨
+      itemBuilder: (context, index) {
+        var webtoon = snapshot.data![index];
+        return Column(
+          children: [
+            SizedBox(
+              width: 200,
+              child: Image.network(
+                webtoon.thumb,
+                // 403 에러 해결 위해 추가
+                headers: const {
+                  'Referer': 'https://comic.naver.com',
+                },
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              webtoon.title,
+              style: const TextStyle(
+                fontSize: 18,
+              ),
+            ),
+          ],
+        );
+      },
+      // 각 아이템 사이에 렌더링됨
+      separatorBuilder: (context, index) => const SizedBox(
+        width: 20,
       ),
     );
   }
