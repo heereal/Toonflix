b3b47697135f8a74b491b77ede49c33719e047e5
heeryoung <winterbooks@naver.com>
2024-05-08 17:28:24 +0900

    refactor: 디테일 스크린 StatelessWidget으로 변경 후 api 메소드 호출

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -3,46 +3,28 @@ import 'package:toonflix/models/webtoon_detail_model.dart';
 import 'package:toonflix/models/webtoon_episode_model.dart';
 import 'package:toonflix/services/api_service.dart';
 
-class DetailScreen extends StatefulWidget {
+class DetailScreen extends StatelessWidget {
   final String title, thumb, id;
 
-  // 어떤 프로퍼티(webtoon)를 초기화할 때 다른 프로터피(id)에는 접근할 수 없음
-  // 메소드 실행을 위해 StatelessWidget -> StatefulWidget으로 변경 (메소드에 argument를 전달해야 하는 경우)
-  // Future<WebtoonDetailModel> webtoon = ApiService.getToonById(id); 불가능
+  late final Future<WebtoonDetailModel> webtoon;
+  late final Future<List<WebtoonEpisodeModel>> episodes;
 
-  const DetailScreen({
+  // 생성자 안에서 초기화를 해 주면 이미 id를 인자로 받아온 상태에서 사용하기 때문에
+  // StatelessWidget에서도 argument가 있는 메소드 사용 가능
+  DetailScreen({
     super.key,
     required this.title,
     required this.thumb,
     required this.id,
-  });
-
-  @override
-  State<DetailScreen> createState() => _DetailScreenState();
-}
-
-// 데이터(title 등 프로퍼티)는 StatefulWidget(DetailScreen)으로 전달됨
-// -> 이 데이터를 State 클래스(_DetailScreenState)에서 받아오기 위해 widget을 참조하는 방법을 사용함
-// widget은 부모 위젯(DetailScreen)을 의미함 -> SatefulWidget의 build 메소드에서는 widget.title로 접근
-class _DetailScreenState extends State<DetailScreen> {
-  // Constructor에서는 widget을 참조할 수 없기 때문에 변수를 선언만 함
-  late Future<WebtoonDetailModel> webtoon;
-  late Future<List<WebtoonEpisodeModel>> episodes;
-
-  @override
-  // initState에서는 widget으로 참조 가능
-  void initState() {
-    super.initState();
-    webtoon = ApiService.getToonById(widget.id);
-    episodes = ApiService.getLatestEpisodesById(widget.id);
-  }
+  })  : webtoon = ApiService.getToonById(id),
+        episodes = ApiService.getLatestEpisodesById(id);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(title),
         centerTitle: true,
         backgroundColor: Colors.indigo[400],
         foregroundColor: Colors.white,
@@ -60,7 +42,7 @@ class _DetailScreenState extends State<DetailScreen> {
               // 화면 전환 시 애니메이션을 제공해주는 위젯
               // Hero 위젯을 두 개의 화면에 각각 추가하고, 각각의 위젯에 같은 태그를 추가하는 방식으로 사용
               Hero(
-                tag: widget.id,
+                tag: id,
                 child: Container(
                   width: 200,
                   // 자식의 부모 영역 침범을 제어함 (BorderRadius 적용 위해 추가)
@@ -76,7 +58,7 @@ class _DetailScreenState extends State<DetailScreen> {
                     ],
                   ),
                   child: Image.network(
-                    widget.thumb,
+                    thumb,
                   ),
                 ),
               ),
