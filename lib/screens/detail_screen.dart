2ecfafbc8433d2106537338891f3cc9bdff97ad4
heeryoung <winterbooks@naver.com>
2024-05-08 17:17:08 +0900

    feat: 디테일 스크린에 api 메소드 호출 코드 추가

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/models/webtoon_detail_model.dart';
+import 'package:toonflix/models/webtoon_episode_model.dart';
+import 'package:toonflix/services/api_service.dart';
 
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
   final String title, thumb, id;
+
+  // 어떤 프로퍼티(webtoon)를 초기화할 때 다른 프로터피(id)에는 접근할 수 없음
+  // 메소드 실행을 위해 StatelessWidget -> StatefulWidget으로 변경 (메소드에 argument를 전달해야 하는 경우)
+  // Future<WebtoonDetailModel> webtoon = ApiService.getToonById(id); 불가능
 
   const DetailScreen({
     super.key,
@@ -11,11 +18,31 @@ class DetailScreen extends StatelessWidget {
   });
 
   @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+// 데이터(title 등 프로퍼티)는 StatefulWidget(DetailScreen)으로 전달됨
+// -> 이 데이터를 State 클래스(_DetailScreenState)에서 받아오기 위해 widget을 참조하는 방법을 사용함
+// widget은 부모 위젯(DetailScreen)을 의미함 -> SatefulWidget의 build 메소드에서는 widget.title로 접근
+class _DetailScreenState extends State<DetailScreen> {
+  // Constructor에서는 widget을 참조할 수 없기 때문에 변수를 선언만 함
+  late Future<WebtoonDetailModel> webtoon;
+  late Future<List<WebtoonEpisodeModel>> episodes;
+
+  @override
+  // initState에서는 widget으로 참조 가능
+  void initState() {
+    super.initState();
+    webtoon = ApiService.getToonById(widget.id);
+    episodes = ApiService.getLatestEpisodesById(widget.id);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
         centerTitle: true,
         backgroundColor: Colors.indigo[400],
         foregroundColor: Colors.white,
@@ -33,7 +60,7 @@ class DetailScreen extends StatelessWidget {
               // 화면 전환 시 애니메이션을 제공해주는 위젯
               // Hero 위젯을 두 개의 화면에 각각 추가하고, 각각의 위젯에 같은 태그를 추가하는 방식으로 사용
               Hero(
-                tag: id,
+                tag: widget.id,
                 child: Container(
                   width: 200,
                   // 자식의 부모 영역 침범을 제어함 (BorderRadius 적용 위해 추가)
@@ -49,7 +76,7 @@ class DetailScreen extends StatelessWidget {
                     ],
                   ),
                   child: Image.network(
-                    thumb,
+                    widget.thumb,
                   ),
                 ),
               ),
